878eb7e04f7a3179e5b1f77b640ded8cfe4d8950
fanisbad98 <101257051+fanisbad98@users.noreply.github.com>
2024-04-11 10:57:03 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,64 @@
 import 'package:bird/game/bird_game.dart';
-import 'package:bird/screens/game_over_sceen.dart';
 import 'package:bird/screens/main_menu_screen.dart';
+import 'package:bird/screens/game_over_sceen.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-Future<void> main() async {
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
 
-final game = BirdGame();
-  runApp(
-    GameWidget(
-      game: game,
-      initialActiveOverlays: const [MainMenuScreen.id],
-      overlayBuilderMap: {
-        'mainMenu': (context, _) => MainMenuScreen(game: game),
-        'gameOver': (context, _) => GameOVerScreen(game: game),
+  final game = BirdGame();
+
+  // Initialize the banner ad object
+  final bannerAd = BannerAd(
+    adUnitId: 'ca-app-pub-3940256099942544/6300978111', // Replace with your Ad Unit ID
+    size: AdSize.banner,
+    request: const AdRequest(),
+    listener: BannerAdListener(
+      onAdFailedToLoad: (ad, error) {
+        print('Failed to load banner ad: $error');
       },
     ),
   );
+
+  // Load the banner ad
+  await bannerAd.load();
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GameWidget(
+              game: game,
+              initialActiveOverlays: const [MainMenuScreen.id],
+              overlayBuilderMap: {
+                'mainMenu': (context, _) => MainMenuScreen(game: game),
+                'gameOver': (context, _) => GameOverScreen(game: game),
+              },
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                width: bannerAd.size.width.toDouble(),
+                height: bannerAd.size.height.toDouble(),
+                child: AdWidget(ad: bannerAd),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
-
-
--- lib/screens/game_over_sceen.dart
+++ lib/screens/game_over_sceen.dart
@@ -1,15 +1,53 @@
+import 'dart:io';
+
 import 'package:bird/game/bird_game.dart';
 import 'package:flutter/material.dart';
 import 'package:bird/game/assets.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-
-class GameOVerScreen extends StatelessWidget {
+class GameOverScreen extends StatefulWidget {
   final BirdGame game;
 
-  const GameOVerScreen({Key? key,required this.game}) : super(key : key);
+  const GameOverScreen({Key? key, required this.game}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  _GameOverScreenState createState() => _GameOverScreenState();
+}
+
+class _GameOverScreenState extends State<GameOverScreen> {
+  
+  InterstitialAd? _interstitialAd;
+  final adUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/1033173712'
+      : 'ca-app-pub-3940256099942544/4411468910';
+
+  @override
+  void initState() {
+    super.initState();
+    loadAd();
+  }
+
+
+  void loadAd() {
+    InterstitialAd.load(
+      adUnitId: adUnitId,
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          debugPrint('$ad loaded.');
+          setState(() {
+            _interstitialAd = ad;
+          });
+        },
+        onAdFailedToLoad: (error) {
+          debugPrint('InterstitialAd failed to load: $error');
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Material(
       color: Colors.black38,
       child: Center(
@@ -17,31 +55,46 @@ class GameOVerScreen extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              'Score: ${game.bird.score}',
+              'Score: ${widget.game.bird.score}',
               style: const TextStyle(
                 fontSize: 60,
                 color: Colors.white,
                 fontFamily: 'Game',
-                ),
+              ),
             ),
             Image.asset(Assets.gameOver),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: onRestart,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
-              child: const Text('Restart',
-              style: TextStyle(fontSize:20),
+              onPressed: () {
+                if (_interstitialAd != null) {
+                  _interstitialAd!.fullScreenContentCallback =
+                      FullScreenContentCallback(
+                    onAdDismissedFullScreenContent: (ad) {
+                      restartGame();
+                    },
+                  );
+                  _interstitialAd!.show();
+                } else {
+                  restartGame();
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+              ),
+              child: const Text(
+                'Restart',
+                style: TextStyle(fontSize: 20),
               ),
             ),
           ],
         ),
-      ),  
+      ),
     );
   }
 
-  void onRestart(){
-    game.bird.reset();
-    game.overlays.remove('gameOver');
-    game.resumeEngine();
+  void restartGame() {
+    widget.game.bird.reset();
+    widget.game.overlays.remove('gameOver');
+    widget.game.resumeEngine();
   }
 }
